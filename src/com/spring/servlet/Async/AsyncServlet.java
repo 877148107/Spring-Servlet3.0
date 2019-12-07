9da87f778478ce4c2fa0f61d120f7827f74eb7e2
William.wangmy <William.wangmy@qq.com>
2019-12-08 00:03:45 +0800

    Servlet3.0

--- src/com/spring/servlet/Async/AsyncServlet.java
+++ src/com/spring/servlet/Async/AsyncServlet.java
@@ -2,6 +2,7 @@ package com.spring.servlet.Async;
 
 import javax.servlet.AsyncContext;
 import javax.servlet.ServletException;
+import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -25,15 +26,32 @@ public class AsyncServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         AsyncContext asyncContext = req.startAsync();
 
+        System.out.println("主线程开始："+Thread.currentThread());
         asyncContext.start(new Runnable() {
             @Override
             public void run() {
+                try {
+                    System.out.println("副线程程开始："+Thread.currentThread());
+                    sayHello();
+                    //获取响应
+                    ServletResponse response = asyncContext.getResponse();
+                    response.getWriter().write("hello  Async...............");
+                    asyncContext.complete();
+                    System.out.println("副线程程结束："+Thread.currentThread());
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
 
             }
         });
+        resp.getWriter().write("hello  Async主线程...............");
+        System.out.println("主线程结束："+Thread.currentThread());
     }
 
-    public void sayHello(){
-
+    public void sayHello() throws InterruptedException {
+        System.out.println(Thread.currentThread()+"...................");
+        Thread.sleep(3000);
     }
 }
